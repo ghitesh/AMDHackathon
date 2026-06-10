2da5dadad9b33ed4880ac2238e4151fe6f13de35
ghitesh <ghitesh@gmail.com>
2026-06-10 18:06:17 +0530

    Create service_catalog.py

---
+++ gitfiles/graph/service_catalog.py
@@ -0,0 +1,14 @@
+AWS_SERVICES = {
+    "S3",
+    "EC2",
+    "Lambda",
+    "CloudFront",
+    "RDS",
+    "ECS",
+    "ALB",
+    "VPC",
+    "API Gateway",
+    "DynamoDB",
+    "SQS",
+    "SNS",
+}
