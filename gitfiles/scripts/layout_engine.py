bfb21facc0cb82b96b3ae85dc2a6bfeffd49a4ab
ghitesh <ghitesh@gmail.com>
2026-06-15 13:57:58 +0530

    Update layout_engine.py

--- gitfiles/scripts/layout_engine.py
+++ gitfiles/scripts/layout_engine.py
@@ -1,14 +1,29 @@
-def calculate_positions(nodes):
-    positions = {}
+from collections import defaultdict
 
-    x = 1.0
+
+LAYER_X_SPACING = 3.0
+LAYER_Y_SPACING = 1.8
+
+
+def calculate_positions(nodes):
+
+    layers = defaultdict(list)
 
     for node in nodes:
-        positions[node["id"]] = {
-            "x": x,
-            "y": 2
-        }
+        layer = node.get("layer", 0)
+        layers[layer].append(node)
 
-        x += 2.5
+    positions = {}
+
+    for layer, layer_nodes in layers.items():
+
+        start_x = 1.0
+
+        for idx, node in enumerate(layer_nodes):
+
+            positions[node["id"]] = {
+                "x": start_x + idx * LAYER_X_SPACING,
+                "y": 1.0 + layer * LAYER_Y_SPACING
+            }
 
     return positions
