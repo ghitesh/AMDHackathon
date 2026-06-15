9aa341743d218f0938fbf1a25dadde6614dbdf73
ghitesh <ghitesh@gmail.com>
2026-06-15 08:17:09 +0000

    Updated icon path

--- gitfiles/scripts/aws_icon_mapper.py
+++ gitfiles/scripts/aws_icon_mapper.py
@@ -2,12 +2,12 @@ from pathlib import Path
 
 
 ICON_MAP = {
-    "EC2": "Amazon-EC2.png",
-    "RDS": "Amazon-RDS.png",
-    "S3": "Amazon-S3.png",
-    "ALB": "Elastic-Load-Balancing.png",
-    "Lambda": "AWS-Lambda.png",
-    "DynamoDB": "Amazon-DynamoDB.png"
+    "EC2": "Architecture-Group-Icons_04302026/EC2-instance-contents_32.png",
+    "RDS": "Resource-Icons_04302026/Res_Databases/Res_Amazon-Aurora_Amazon-RDS-Instance_48.png",
+    "S3": "Architecture-Service-Icons_04302026/Arch_Storage/48/Arch_Amazon-Simple-Storage-Service_48.png",
+    "ALB": "Resource-Icons_04302026/Res_Networking-Content-Delivery/Res_Elastic-Load-Balancing_Network-Load-Balancer_48.png",
+    "Lambda": "Resource-Icons_04302026/Res_Compute/Res_AWS-Lambda_Lambda-Function_48.png",
+    "DynamoDB": "Architecture-Service-Icons_04302026/Arch_Databases/32/Arch_Amazon-DynamoDB_32.png"
 }
 
 
